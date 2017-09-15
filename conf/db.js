3ea5bc4522b4879875ea6b70188a67e0f0899d13
宋俊龙 <songjunlong@air-x.com.cn>
2017-09-15 10:04:36 +0800

    cm

--- conf/db.js
+++ conf/db.js
@@ -2,8 +2,8 @@ module.exports = {
 	mysql: {
 		host: '192.168.1.165',
 		user: 'root',
-		password: '0061204228',
-		database: 'airx',
+		password: '1qaz2wsx',
+		database: 'order_admin_db',
 		port: 3306
 	}
 }
